ed0b644ed537291b4ca39a5b966807e74b41a9ac
aerhergag0 <aerhergag@naver.com>
2021-06-10 20:52:01 +0900

    0610 finish

---
+++ java-basic/app/src/main/java/com/eomcs/lang/Exam01/Exam1_2.java
@@ -0,0 +1,10 @@
+package com.eomcs.lang.ex01;
+
+// 목표 : 한 소스 파일에 여러 개의 클래스가 있을 때의 컴파일
+
+class A {}
+class B {}
+class C {}
+
+// 결과 : C:\Users\rmrdo\git\bitcamp-study\java-basic\app\bin\main\com\eomcs\lang\ex01 에
+// A.class , B.class, C.class 파일 각각 3개 생성됨
